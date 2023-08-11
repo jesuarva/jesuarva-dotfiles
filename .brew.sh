ff9938ff8a25c7ec316c3a0074a0aed422364aca
jean.ariza <jesuarva@gmail.com>
2023-08-11 17:27:31 +0100

    Homebrew setup

---
+++ .brew.sh
@@ -0,0 +1,17 @@
+## ❗️ **line below** Only needed in Mac with new MX ship
+# eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# COMPLETION SCRIPT
+if type brew &>/dev/null
+then
+  HOMEBREW_PREFIX="$(brew --prefix)"
+  if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]
+  then
+    source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
+  else
+    for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*
+    do
+      [[ -r "${COMPLETION}" ]] && source "${COMPLETION}"
+    done
+  fi
+fi
